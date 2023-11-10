826bf68915ebdaf77a5e484ac9d052fcab2878db
Abigail Paredes <abigailparedes07@gmail.com>
2023-11-10 11:25:27 -0300

    modifique la estructura del registro agregue una funcion para tomar los datos

--- js/registro_estudiantil.js
+++ js/registro_estudiantil.js
@@ -1,3 +1,4 @@
+
 class RegistroEstudiantil {
     USUARIOS = [];
 
@@ -5,13 +6,13 @@ class RegistroEstudiantil {
         
     }
 
-    static crearUsuario(nombre, apellido, email, rol) {
-        const usr = { id: this.generarID(), nombre, apellido, email, rol };
+    static crearUsuario(nombre, apellido, rol, email) {
+        const usr = { id: this.generarID(), nombre, apellido, rol, email };
         return usr;
     }
 
-    static crearEstudiante(nombre, apellido, email, rol, edad, paisNac, codCuatrimIni) {
-        const usr = this.crearUsuario(nombre, apellido, email, rol);
+    static crearEstudiante(nombre, apellido, rol, email, edad, paisNac, codCuatrimIni) {
+        const usr = this.crearUsuario(nombre, apellido, rol, email);
         usr.edad = edad;
         usr.paisNac = paisNac;
         usr.codCuatrimIni = codCuatrimIni;
@@ -27,6 +28,46 @@ class RegistroEstudiantil {
     }
 }
 
+// recibe la infomacion del nuevo estudiante y la "añade" al arreglo.
+document.addEventListener('DOMContentLoaded',()=>{
+const registrar= new RegistroEstudiantil();
+
+const formulario = document.getElementById('form-registro');
+
+formulario.addEventListener('submit', function(event){
+event.preventDefault();
+const nombre= document.getElementById('nombre').value;
+const apellido= document.getElementById('apellido').value;
+const rol= document.getElementById('select-rol').value;
+const email= document.getElementById('email-alumno').value;
+const edad= document.getElementById('edad').value;
+const paisNac= document.getElementById('select-paises').value;
+const codCuatrimIni= document.getElementById('cuatrimestre').value;
+
+const nuevoEstudiante= RegistroEstudiantil.crearEstudiante(nombre, apellido, rol, email, edad , paisNac , codCuatrimIni);
+
+// al nuevo estudiante que estamos registrando le generamos un id
+nuevoEstudiante.id = RegistroEstudiantil.generarID(); 
+
+//pusheamos el estudiante al arreglo usuarios
+if(registrar.USUARIOS && registrar.USUARIOS.length < 10){
+
+    registrar.USUARIOS.push(nuevoEstudiante);
+} else {
+
+    console.log("No se pueden ingresar más alumnos")
+}
+
+   formulario.reset();
+   console.log(registrar.USUARIOS)
+
+})
+
+});
+
+
+
+///
 if (window.location.href.toString().includes('registro_estudiantil.html')) {
     const registro = new RegistroEstudiantil()
 }
