8b7feb95e2193711f1e03b8c38977eb72ea4ab35
Helckya <bouquetyg@gmail.com>
2023-11-15 19:19:54 -0300

    el registro de estudiantes se refleja en local storage

--- modulos/registro/registro_estudiantil.js
+++ modulos/registro/registro_estudiantil.js
@@ -13,7 +13,9 @@ class RegistroEstudiantil {
         btn_materias.addEventListener('click',e =>{
             e.preventDefault();
             window.location.href= '../materias/materias.html';
-        })        
+        });
+
+        this.USUARIOS = JSON.parse(localStorage.getItem("usersList"));
     }
 
     static crearUsuario(usuario, password, nombre, apellido, rol, email) {
@@ -56,24 +58,27 @@ class RegistroEstudiantil {
         const formulario = document.getElementById('form-registro');
         const nombre = document.getElementById('nombre').value;
         const apellido = document.getElementById('apellido').value;
+        const usuario = document.getElementById('usuario').value;
+        const contrasenia = document.getElementById('contrasenia').value;
         const rol = document.getElementById('select-rol').value;
         const email = document.getElementById('email-alumno').value;
         const edad = document.getElementById('edad').value;
         const paisNac = document.getElementById('select-paises').value;
         const codCuatrimIni = document.getElementById('cuatrimestre').value;
 
-        const nuevoEstudiante = RegistroEstudiantil.crearEstudiante(nombre, apellido, rol, email, edad, paisNac, codCuatrimIni);
-
         // Comprobar que no se repita el email
         
         //pusheamos el estudiante al arreglo usuarios
         if (this.USUARIOS && this.USUARIOS.length < 10) {
+            const nuevoEstudiante = RegistroEstudiantil.crearEstudiante(usuario, contrasenia, nombre, apellido, rol, email, edad, paisNac, codCuatrimIni);
             this.USUARIOS.push(nuevoEstudiante);
+            localStorage.setItem('usersList', JSON.stringify(this.USUARIOS));
+            
             containerMsgs.style = 'background-color: green';
-            containerMsgs.innerHTML = 'USUARIO REGISTRADO EXITOSAMENTE'
+            containerMsgs.innerHTML = 'USUARIO REGISTRADO EXITOSAMENTE';
         } else {
             containerMsgs.style = 'background-color: red';
-            containerMsgs.innerHTML = 'NO SE PUEDEN REGISTAR MAS ALUMNOS'
+            containerMsgs.innerHTML = 'NO SE PUEDEN REGISTAR MAS ALUMNOS';
         }
 
         console.log(this.USUARIOS)
