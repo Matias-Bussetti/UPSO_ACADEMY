e1d37098f68c5cfd063ae876932a5d271bc9332e
Matias-Bussetti <desarrollos.informaticos.bsstt@gmail.com>
2023-11-28 19:18:16 -0300

    utils finish

--- utils/utils.js
+++ utils/utils.js
@@ -1,20 +1,45 @@
- crearUsuario(usuario, password, nombre, apellido, rol, email) {
-    const usr = { id: this.generarID(), usuario, password, nombre, apellido, rol, email };
-    return usr;
+function crearUsuario(usuario, password, nombre, apellido, rol, email) {
+  const usr = {
+    id: this.generarID(),
+    usuario,
+    password,
+    nombre,
+    apellido,
+    rol,
+    email,
+  };
+  return usr;
 }
 
- crearEstudiante(usuario, password, nombre, apellido, rol, email, edad, paisNac, codCuatrimIni) {
-    const usr = this.crearUsuario(usuario, password, nombre, apellido, rol, email);
-    usr.edad = edad;
-    usr.paisNac = paisNac;
-    usr.codCuatrimIni = codCuatrimIni;
+function crearEstudiante(
+  usuario,
+  password,
+  nombre,
+  apellido,
+  rol,
+  email,
+  edad,
+  paisNac,
+  codCuatrimIni
+) {
+  const usr = this.crearUsuario(
+    usuario,
+    password,
+    nombre,
+    apellido,
+    rol,
+    email
+  );
+  usr.edad = edad;
+  usr.paisNac = paisNac;
+  usr.codCuatrimIni = codCuatrimIni;
 
-    return usr;
+  return usr;
 }
 
- generarID() {
-    // Genera un ID a partir de una cadena de texto generada aleatoreamente
-    const stringRandom = Math.random().toString(36).substring(2, 10);
+function generarID() {
+  // Genera un ID a partir de una cadena de texto generada aleatoreamente
+  const stringRandom = Math.random().toString(36).substring(2, 10);
 
-    return stringRandom;
+  return stringRandom;
 }
